44ca606c7cc6017634d30616b2b55e6a6aaa7974
ariel-delgado <62970978+ariel-delgado@users.noreply.github.com>
2022-04-05 14:32:50 -0500

    Add files via upload

---
+++ testscript.ps1
@@ -0,0 +1,3 @@
+﻿mkdir C:\users\ariel_delgado\Desktop\scripttest
+
+wget "https://go.microsoft.com/fwlink/?linkid=2069324&language=en-us&Consent=1" -OutFile C:\users\ariel_delgado\Desktop\scripttest\edge.zip
